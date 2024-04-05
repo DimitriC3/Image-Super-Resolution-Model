edb290adda7180143e6f17ea98d04848ea4013dc
Dimitri Christopoulos <154035020+DimitriC3@users.noreply.github.com>
2024-04-05 00:42:44 -0400

    Update train.py

--- train.py
+++ train.py
@@ -8,38 +8,6 @@ from torchvision.utils import save_image
 from model.super_resolution import SuperResolutionAutoencoder  # Assuming you have defined your model in a separate file called model.py
 from PIL import Image
 
-#weight directory
-weights_dir = "G:\\My Drive\\weights"
-if not os.path.exists(weights_dir):
-    os.makedirs(weights_dir)
-
-# Define a custom dataset class
-class BlurredImageDataset(Dataset):
-    def __init__(self, root_dir_blurred, root_dir_non_blurred, transform=None):
-        self.root_dir_blurred = root_dir_blurred
-        self.root_dir_non_blurred = root_dir_non_blurred
-        self.blurred_images = os.listdir(root_dir_blurred)
-        self.non_blurred_images = os.listdir(root_dir_non_blurred)
-        self.transform = transform
-
-    def __len__(self):
-        return min(len(self.blurred_images), len(self.non_blurred_images))
-
-    def __getitem__(self, idx):
-        blurred_img_name = os.path.join(self.root_dir_blurred, self.blurred_images[idx])
-        non_blurred_img_name = os.path.join(self.root_dir_non_blurred, self.non_blurred_images[idx])
-        
-        blurred_img = Image.open(blurred_img_name).convert('RGB')
-        non_blurred_img = Image.open(non_blurred_img_name).convert('RGB')
-
-        if self.transform:
-            blurred_img = self.transform(blurred_img)
-            non_blurred_img = self.transform(non_blurred_img)
-
-        return blurred_img, non_blurred_img
-
-
-
 # Set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
