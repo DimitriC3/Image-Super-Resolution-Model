5f31e2ab3b3d99b64f0e0132dbba5106d81e5d5f
bArnauld <157979183+bArnauld@users.noreply.github.com>
2024-04-05 00:35:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,26 +10,10 @@ from flask_cors import CORS
 
 from model import SuperResolutionAutoencoder
 
-def preprocess_image(image):
-    """
-    transform = transforms.Compose([
-        #transforms.Resize((178, 218)),  # Resize to match model input size
-        transforms.ToTensor()          # Convert to tensor
-        #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize
-    ])
-    #image = transform(image).unsqueeze(0)  # Add batch dimension
-    image = transform(image)
-    """
-
-    transform = transforms.ToTensor()     
-    image = transform(image).unsqueeze(0)
-    return image
-
 
 model = SuperResolutionAutoencoder()
 model.load_state_dict(torch.load('overfit_weights/super_resolution_autoencoder_epoch_6.pth'))
 model.eval()
-#model.load_state_dict(torch.load('weights10.pth'))
 
 app = Flask(__name__)
 CORS(app)
@@ -43,35 +27,19 @@ def index():
 def data():
     im = Image.open(io.BytesIO(request.data)).convert("RGB")
     blurred_image = im.filter(ImageFilter.GaussianBlur(2))
-    #blurred_image.show()
 
-    input_image = preprocess_image(blurred_image)
-    #input_image = preprocess_image(im)
+    transform = transforms.ToTensor()     
+    input_tensor = transform(blurred_image).unsqueeze(0)
 
     with torch.no_grad():
-        output_image = model(input_image)
+        output_tensor = model(input_tensor)
 
-    output_image = model(input_image)
-    #print(type(output_image))
+    output_image = transforms.ToPILImage()(output_tensor.squeeze(0))
 
-    # Display or save the output image
-    output_image = transforms.ToPILImage()(output_image.squeeze(0))
-
-
-    #size from CELEB A dataset
-    #width, height = 178, 218
-    #im = im.resize((width, height))
     img_byte_array = io.BytesIO()
     output_image.save(img_byte_array, format='JPEG', quality=100)
     img_byte_array.seek(0)
 
-
-    # Convert the processed image back to bytes
-    """
-    img_byte_array = io.BytesIO()
-    im.save(img_byte_array, format='JPEG')
-    img_byte_array.seek(0)
-    """
     # Return the processed image as a response
     return Response(img_byte_array, mimetype='image/jpeg')
     
